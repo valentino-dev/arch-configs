9df27bf36c952f48816b3e1da32b3a4cb5c21a6b
valentino-dev <angelo.valentino.brade@gmail.com>
2024-11-23 18:21:37 +0100

    ds2

---
+++ desktop2/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,24 @@
+return{
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+  config = function()
+     vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+
+    require("neo-tree").setup({
+      default_component_configs = {
+        file_size = {
+          enabled = true,
+          required_width = 10,
+        },
+      },
+    })
+  end
+	
+}
+
+
